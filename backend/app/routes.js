79ff81e704ace7a7b079173f3d194e8c57594a43
ak1103dev <ak.apichan1103@gmail.com>
2016-04-04 22:07:08 +0700

    use promise

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -4,19 +4,19 @@ module.exports = function(app){
 	// 	res.send("Hello world");
 	// });
 
-
 	app.get('/trips', function(req, res){
 		console.log("list all trips");
-		Trip.find({}, '_id trip.name', function(err, trips) {
-			if(err)
-				console.log(err);
-			if(trips)
-				res.send(trips);
-			else {
-				res.send("no any trip");
-			}
+		Trip.find({}, '_id trip.name')
+			.then(function(trips) {
+				if(trips)
+					res.send(trips);
+				else
+					res.send("no any trip");
+			})
+			.catch(function(err) {
+				res.send(err);
+			});
 		});
-	});
 
 	app.post('/trips', function(req, res){
 		console.log("create new trip");
@@ -33,16 +33,17 @@ module.exports = function(app){
 
 	app.get('/trips/:id', function(req, res){
 		console.log('get one trip');
-		Trip.findOne({ '_id': req.params.id }, function(err, trip) {
-			if(err)
-				console.log(err);
-			if(trip)
-				res.send(trip);
-			else {
-				res.send("no this trip");
-			}
+		Trip.findOne({ '_id': req.params.id })
+			.then(function(trip) {
+				if(trip)
+					res.send(trip);
+				else
+					res.send("no this trip");
+			})
+			.catch(function(err) {
+				res.send(err);
+			});
 		});
-	});
 
 	app.put('/trips/:id', function(req, res){
 		console.log("update trip")
@@ -51,24 +52,25 @@ module.exports = function(app){
 			'trip.price': req.body.price,
 			'trip.description': req.body.description
 		};
-		Trip.update({ '_id': req.params.id }, update, function(err) {
-			if(err)
-				throw err;
+		Trip.update({ '_id': req.params.id }, update)
+			.then(function(err) {
+				if(err)
+					throw err;
+			});
+			res.send('updated');
 		});
-		res.send('updated');
-	});
 
 	app.delete('/trips/:id', function(req, res){
 		console.log("delete trip");
-		Trip.remove({ '_id': req.params.id }, function(err, trip) {
-			if(err)
-				console.log(err);
-			if(trip) {
-				res.send(trip);
-			}
-			else {
-				res.send("can not delete");
-			}
+		Trip.remove({ '_id': req.params.id })
+			.then(function(trip) {
+				if(trip)
+					res.send(trip);
+				else
+					res.send("can not delete");
+			})
+			.catch(function(err) {
+				res.send(err);
+			});
 		});
-	});
 };
