888ff1641413b1bf005e8e7fdd78033c93a31246
ak1103 <ak.apichan1103@gmail.com>
2016-04-24 12:57:16 +0700

    render image and text

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -1,5 +1,5 @@
 var url = "https://www.takemetour.com/api/home";
-var img = "https://d2ad32w75yf016.cloudfront.net/inspiration/original/";
+var img_url = "https://d2ad32w75yf016.cloudfront.net/inspiration/original/";
 $.get(url, function(data) {
   //console.log(data);
   var inspiration = data.inspiration_layouts;
@@ -9,9 +9,16 @@ $.get(url, function(data) {
 	//console.log(row);
 	for(var j = 0; j < row.length; j++) {
 	  var col = row[j].inspiration_id;
-	  console.log(col);
-	  console.log("name: " + col.name);
-	  console.log("image: " + col.thumbnail_media_id.file_name);
+	  var name = col.name;
+	  var image = col.thumbnail_media_id.file_name
+	  //console.log(col);
+	  console.log("name: " + name);
+	  console.log("image: " + image);
+	  var tag_img = '<a href="#"><img class="img-responsive" src="' + img_url + image + '"/></a>';
+	  var tag_text = `<div class="text">
+         				<h2>` + name + `</h2>
+      				  </div>`
+	  $("#"+ i + j).html(tag_img + tag_text);
 	}
   }
 });
