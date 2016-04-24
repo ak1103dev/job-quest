7b8aeed197b9091b42d538a99c9364c2a867c984
ak1103 <ak.apichan1103@gmail.com>
2016-04-24 11:45:34 +0700

    get data for render

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -1,4 +1,18 @@
 var url = "https://www.takemetour.com/api/home";
+var img = "https://d2ad32w75yf016.cloudfront.net/inspiration/original/";
 $.get(url, function(data) {
-  console.log(data);
+  //console.log(data);
+  var inspiration = data.inspiration_layouts;
+  //console.log(inspiration);
+  for(var i = 0; i < inspiration.length; i++) {
+	var row = inspiration[i].columns;
+	//console.log(row);
+	for(var j = 0; j < row.length; j++) {
+	  var col = row[j].inspiration_id;
+	  console.log(col);
+	  console.log("name: " + col.name);
+	  console.log("image: " + col.thumbnail_media_id.file_name);
+	}
+  }
 });
+
